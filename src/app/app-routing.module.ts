4133af589fd2a618428d09539b431797481fad17
Quentin <quentin.garreau72@gmail.com>
2022-09-12 09:11:48 +0200

    GeoFrance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommuneComponent } from './components/commune/commune.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'commune', component: CommuneComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
